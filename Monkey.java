c2fb342d3fcc8de282a7df664c4a9d4b015d6093
junli7 <2007junli@gmail.com>
2025-05-15 17:44:52 -0400

    commit

---
+++ Monkey.java
@@ -0,0 +1,17 @@
+import java.util.List;
+public class Monkey {
+    private int x;
+    private int y;
+    private double range;
+    private double hitbox;
+
+    private int level;
+
+    public Monkey(int x, int y, double range, double hitbox, double level){
+        
+
+    }
+
+
+
+}
